865142f341dd85cbfdfeec56566163688be0e47d
ezhr <ezhr>
2016-12-18 03:13:55 +0300

    Result class to handle responses from server

---
+++ app/src/main/java/com/example/ezhr/notesmagotes/models/Result.java
@@ -0,0 +1,29 @@
+package com.example.ezhr.notesmagotes.models;
+
+/**
+ * Created by EzhR on 12/16/2016.
+ */
+
+public class Result {
+
+    String success;
+    String message;
+    String token;
+    int status;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
